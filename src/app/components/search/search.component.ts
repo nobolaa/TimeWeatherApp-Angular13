d24f0d2a02ee22a4c760a13e6f137d360e62c9f0
nobolaa <nabilaliasserhaou@gmail.com>
2022-04-29 01:42:12 +0200

    Added formControl on search component. Added function onChange to filter when input changes and emit the value. Added styles to search component.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
+  template: `
+    <div class="search">
+      <input class="search__input" placeholder="City..." [formControl]="inputSearch">
+    </div>
+  `,
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  inputSearch = new FormControl('');
+  @Output() submitted = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.onChange();
   }
 
+  private onChange(): void{
+    this.inputSearch.valueChanges
+    .pipe(
+      map( (search) => search.trim()),
+      debounceTime(500),
+      distinctUntilChanged(),
+      filter( (search) => search!=''),
+      tap((search) => this.submitted.emit(search))
+    )
+    .subscribe();
+  }
 }
